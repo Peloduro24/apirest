e8498bc48dc793ec24e3455adc7d7270ffec4cba
Peloduro24 <gonzalete0024@gmail.com>
2025-09-15 01:19:31 -0500

    cambio

--- handler.js
+++ handler.js
@@ -1,46 +1,39 @@
 'use strict';
 let AWS = require("aws-sdk");
+const mysql = require('mysql2/promise');
 
 module.exports.hello = async (event) => {
   AWS.config.update({ region: "us-east-1" });
 
   const docClient = new AWS.DynamoDB.DocumentClient();
 
-  let bodyprubea = ""
   const params = {
     TableName: "TEST",
   };
 
+  const connection = await mysql.createPool({
+    host: "bareq14istuf7vmjke0a-mysql.services.clever-cloud.com",
+    user: "uiexpfrazy0okyo1",
+    password: "EOqbRUqzoWE0EtbnDiTY",
+    database: "bareq14istuf7vmjke0a",
+  });
+  console.log("CONEXION MYSQL CREADA", connection)
+
   try {
+    const [rows] = await connection.execute('SELECT * FROM Usuario');
+
+    console.log("DATOS DE MYSQL CREADAS", rows);
     const data = await docClient.scan(params).promise();
 
     console.log("Items encontrados:", data.Items);
-    if (
-      event.requestContext.http.method === "POST"
-    ) {
-      console.log(JSON.parse(event.body), "as")
-      const params1 = JSON.parse(event.body)
-      const params = {
-        TableName: "TEST",
-        Item: {
-          id: params1.payload.id,
-          nombre: params1.payload.nombre
-        }
-      }
-      await docClient.put(params).promise();
-      bodyprubea = event.requestContext.http
-    }
 
     return {
-      test: "scan",
       statusCode: 200,
       body: JSON.stringify(
         {
           message: 'Scan ejecutado correctamente',
           items: data.Items,
-          event: event,
-          bodyprubea
-
+          respuestaMysql: JSON.stringify(rows)
         },
         null,
         2
@@ -61,5 +54,7 @@ module.exports.hello = async (event) => {
       ),
     };
   }
-
+  finally {
+    await connection.end(); // cerrar conexión
+  }
 };
